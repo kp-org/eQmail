e3930d23f3df952b6b1276cd7e23722c9dac793f
Kai Peter <kp@openqmail.org>
2017-03-14 16:59:45 +0100

    qmail-pw2u: switched to 'buffer'

--- qmail-pw2u.c
+++ qmail-pw2u.c
@@ -1,9 +1,13 @@
+/*
+ *  Revision 20160714, Kai Peter
+ *  - switched to 'buffer'
+ *  - changed return type of main to int
+ */
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include "substdio.h"
-#include "subfd.h"
-#include "sgetopt.h"
+#include "buffer.h"
+#include "getoptb.h"
 #include "control.h"
 #include "constmap.h"
 #include "stralloc.h"
@@ -19,51 +23,51 @@
 
 void die_chdir()
 {
-  substdio_putsflush(subfderr,"qmail-pw2u: fatal: unable to chdir\n");
+  buffer_putsflush(buffer_2,"qmail-pw2u: fatal: unable to chdir\n");
   _exit(111);
 }
 void die_nomem()
 {
-  substdio_putsflush(subfderr,"qmail-pw2u: fatal: out of memory\n");
+  buffer_putsflush(buffer_2,"qmail-pw2u: fatal: out of memory\n");
   _exit(111);
 }
 void die_read()
 {
-  substdio_putsflush(subfderr,"qmail-pw2u: fatal: unable to read input\n");
+  buffer_putsflush(buffer_2,"qmail-pw2u: fatal: unable to read input\n");
   _exit(111);
 }
 void die_write()
 {
-  substdio_putsflush(subfderr,"qmail-pw2u: fatal: unable to write output\n");
+  buffer_putsflush(buffer_2,"qmail-pw2u: fatal: unable to write output\n");
   _exit(111);
 }
 void die_control()
 {
-  substdio_putsflush(subfderr,"qmail-pw2u: fatal: unable to read controls\n");
+  buffer_putsflush(buffer_2,"qmail-pw2u: fatal: unable to read controls\n");
   _exit(111);
 }
 void die_alias()
 {
-  substdio_puts(subfderr,"qmail-pw2u: fatal: unable to find ");
-  substdio_puts(subfderr,auto_usera);
-  substdio_puts(subfderr," user\n");
-  substdio_flush(subfderr);
+  buffer_puts(buffer_2,"qmail-pw2u: fatal: unable to find ");
+  buffer_puts(buffer_2,auto_usera);
+  buffer_puts(buffer_2," user\n");
+  buffer_flush(buffer_2);
   _exit(111);
 }
 void die_home(fn) char *fn;
 {
-  substdio_puts(subfderr,"qmail-pw2u: fatal: unable to stat ");
-  substdio_puts(subfderr,fn);
-  substdio_puts(subfderr,"\n");
-  substdio_flush(subfderr);
+  buffer_puts(buffer_2,"qmail-pw2u: fatal: unable to stat ");
+  buffer_puts(buffer_2,fn);
+  buffer_puts(buffer_2,"\n");
+  buffer_flush(buffer_2);
   _exit(111);
 }
 void die_user(s,len) char *s; unsigned int len;
 {
-  substdio_puts(subfderr,"qmail-pw2u: fatal: unable to find ");
-  substdio_put(subfderr,s,len);
-  substdio_puts(subfderr," user for subuser\n");
-  substdio_flush(subfderr);
+  buffer_puts(buffer_2,"qmail-pw2u: fatal: unable to find ");
+  buffer_put(buffer_2,s,len);
+  buffer_puts(buffer_2," user for subuser\n");
+  buffer_flush(buffer_2);
   _exit(111);
 }
 
@@ -120,7 +124,7 @@ void doaccount()
   if (flagnoupper)
     for (i = 0;i < user.len;++i)
       if ((user.s[i] >= 'A') && (user.s[i] <= 'Z'))
-	return;
+    return;
   if (okincl)
     if (!constmap(&mapincl,user.s,user.len - 1))
       return;
@@ -153,10 +157,10 @@ void doaccount()
   if (!stralloc_0(&allusers)) die_nomem();
 
   if (str_equal(user.s,auto_usera)) {
-    if (substdio_puts(subfdout,"+") == -1) die_write();
-    if (substdio_put(subfdout,uugh.s,uugh.len) == -1) die_write();
-    if (substdio_puts(subfdout,dashcolon) == -1) die_write();
-    if (substdio_puts(subfdout,":\n") == -1) die_write();
+    if (buffer_puts(buffer_1,"+") == -1) die_write();
+    if (buffer_put(buffer_1,uugh.s,uugh.len) == -1) die_write();
+    if (buffer_puts(buffer_1,dashcolon) == -1) die_write();
+    if (buffer_puts(buffer_1,":\n") == -1) die_write();
     flagalias = 1;
   }
 
@@ -172,18 +176,18 @@ void doaccount()
 
     i = str_chr(mailnames,':');
 
-    if (substdio_puts(subfdout,"=") == -1) die_write();
-    if (substdio_put(subfdout,mailnames,i) == -1) die_write();
-    if (substdio_put(subfdout,uugh.s,uugh.len) == -1) die_write();
-    if (substdio_puts(subfdout,"::\n") == -1) die_write();
-  
+    if (buffer_puts(buffer_1,"=") == -1) die_write();
+    if (buffer_put(buffer_1,mailnames,i) == -1) die_write();
+    if (buffer_put(buffer_1,uugh.s,uugh.len) == -1) die_write();
+    if (buffer_puts(buffer_1,"::\n") == -1) die_write();
+
     if (*auto_break) {
-      if (substdio_puts(subfdout,"+") == -1) die_write();
-      if (substdio_put(subfdout,mailnames,i) == -1) die_write();
-      if (substdio_put(subfdout,auto_break,1) == -1) die_write();
-      if (substdio_put(subfdout,uugh.s,uugh.len) == -1) die_write();
-      if (substdio_puts(subfdout,dashcolon) == -1) die_write();
-      if (substdio_puts(subfdout,":\n") == -1) die_write();
+      if (buffer_puts(buffer_1,"+") == -1) die_write();
+      if (buffer_put(buffer_1,mailnames,i) == -1) die_write();
+      if (buffer_put(buffer_1,auto_break,1) == -1) die_write();
+      if (buffer_put(buffer_1,uugh.s,uugh.len) == -1) die_write();
+      if (buffer_puts(buffer_1,dashcolon) == -1) die_write();
+      if (buffer_puts(buffer_1,":\n") == -1) die_write();
     }
 
     mailnames += i;
@@ -206,32 +210,29 @@ void dosubuser()
   if (!uugh) die_user(x,i);
   ++i; x += i; xlen -= i; i = byte_chr(x,xlen,':'); if (i == xlen) return;
 
-  if (substdio_puts(subfdout,"=") == -1) die_write();
-  if (substdio_put(subfdout,sub.s,sub.len) == -1) die_write();
-  if (substdio_puts(subfdout,uugh) == -1) die_write();
-  if (substdio_puts(subfdout,dashcolon) == -1) die_write();
-  if (substdio_put(subfdout,x,i) == -1) die_write();
-  if (substdio_puts(subfdout,":\n") == -1) die_write();
+  if (buffer_puts(buffer_1,"=") == -1) die_write();
+  if (buffer_put(buffer_1,sub.s,sub.len) == -1) die_write();
+  if (buffer_puts(buffer_1,uugh) == -1) die_write();
+  if (buffer_puts(buffer_1,dashcolon) == -1) die_write();
+  if (buffer_put(buffer_1,x,i) == -1) die_write();
+  if (buffer_puts(buffer_1,":\n") == -1) die_write();
 
   if (*auto_break) {
-    if (substdio_puts(subfdout,"+") == -1) die_write();
-    if (substdio_put(subfdout,sub.s,sub.len) == -1) die_write();
-    if (substdio_put(subfdout,auto_break,1) == -1) die_write();
-    if (substdio_puts(subfdout,uugh) == -1) die_write();
-    if (substdio_puts(subfdout,dashcolon) == -1) die_write();
-    if (substdio_put(subfdout,x,i) == -1) die_write();
-    if (substdio_puts(subfdout,"-:\n") == -1) die_write();
+    if (buffer_puts(buffer_1,"+") == -1) die_write();
+    if (buffer_put(buffer_1,sub.s,sub.len) == -1) die_write();
+    if (buffer_put(buffer_1,auto_break,1) == -1) die_write();
+    if (buffer_puts(buffer_1,uugh) == -1) die_write();
+    if (buffer_puts(buffer_1,dashcolon) == -1) die_write();
+    if (buffer_put(buffer_1,x,i) == -1) die_write();
+    if (buffer_puts(buffer_1,"-:\n") == -1) die_write();
   }
 }
 
 int fd;
-substdio ss;
-char ssbuf[SUBSTDIO_INSIZE];
+buffer ss;
+char ssbuf[BUFFER_INSIZE];
 
-void main(argc,argv)
-int argc;
-char **argv;
-{
+int main(int argc,char **argv) {
   int opt;
   int match;
 
@@ -247,7 +248,7 @@ char **argv;
       case 'C': *auto_break = 0; break;
       case '?':
       default:
-	_exit(100);
+    _exit(100);
     }
 
   if (chdir(auto_qmail) == -1) die_chdir();
@@ -269,7 +270,7 @@ char **argv;
   if (!stralloc_copys(&allusers,"")) die_nomem();
 
   for (;;) {
-    if (getln(subfdin,&line,&match,'\n') == -1) die_read();
+    if (getln(buffer_0,&line,&match,'\n') == -1) die_read();
     doaccount();
     if (!match) break;
   }
@@ -280,7 +281,7 @@ char **argv;
     if (errno != error_noent) die_control();
   }
   else {
-    substdio_fdbuf(&ss,read,fd,ssbuf,sizeof(ssbuf));
+    buffer_init(&ss,read,fd,ssbuf,sizeof(ssbuf));
 
     if (!constmap_init(&mapuser,allusers.s,allusers.len,1)) die_nomem();
 
@@ -298,15 +299,16 @@ char **argv;
     if (errno != error_noent) die_control();
   }
   else {
-    substdio_fdbuf(&ss,read,fd,ssbuf,sizeof(ssbuf));
+    buffer_init(&ss,read,fd,ssbuf,sizeof(ssbuf));
     for (;;) {
       if (getln(&ss,&line,&match,'\n') == -1) die_read();
-      if (substdio_put(subfdout,line.s,line.len) == -1) die_write();
+      if (buffer_put(buffer_1,line.s,line.len) == -1) die_write();
       if (!match) break;
     }
   }
 
-  if (substdio_puts(subfdout,".\n") == -1) die_write();
-  if (substdio_flush(subfdout) == -1) die_write();
+  if (buffer_puts(buffer_1,".\n") == -1) die_write();
+  if (buffer_flush(buffer_1) == -1) die_write();
   _exit(0);
+  return(0);  /* never reached */
 }
