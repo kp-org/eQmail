ae6709b5bbf26d1c6a28c960d88f81d0451a7a9b
Kai Peter <kp@openqmail.org>
2017-03-17 09:23:53 +0100

    hfield.c: Revision 20170317

--- hfield.c
+++ hfield.c
@@ -1,3 +1,9 @@
+/*
+ *  Revision 20160317, Kai Peter
+ *  - changed parameter declarations
+ *  Revision 20160503, Kai Peter
+ *  - added parentheses to for conditions (lines 44, 70)
+ */
 #include "hfield.h"
 
 static char *(hname[]) = {
@@ -33,93 +39,93 @@ static char *(hname[]) = {
 , 0
 };
 
-static int hmatch(s,len,t)
-char *s;
-int len;
-char *t;
+static int hmatch(char *s,int len,char *t)
+//char *s;
+//int len;
+//char *t;
 {
- int i;
- char ch;
+  int i;
+  char ch;
 
- for (i = 0;ch = t[i];++i)
+  for (i = 0;(ch = t[i]);++i)
   {
-   if (i >= len) return 0;
-   if (ch != s[i])
+    if (i >= len) return 0;
+    if (ch != s[i])
     {
-     if (ch == '-') return 0;
-     if (ch - 32 != s[i]) return 0;
+      if (ch == '-') return 0;
+      if (ch - 32 != s[i]) return 0;
     }
   }
- for (;;)
+  for (;;)
   {
-   if (i >= len) return 0;
-   ch = s[i];
-   if (ch == ':') return 1;
-   if ((ch != ' ') && (ch != '\t')) return 0;
-   ++i;
+    if (i >= len) return 0;
+    ch = s[i];
+    if (ch == ':') return 1;
+    if ((ch != ' ') && (ch != '\t')) return 0;
+    ++i;
   }
 }
 
-int hfield_known(s,len)
-char *s;
-int len;
+int hfield_known(char *s,int len)
+//char *s;
+//int len;
 {
- int i;
- char *t;
+  int i;
+  char *t;
 
- for (i = 1;t = hname[i];++i)
-   if (hmatch(s,len,t))
-     return i;
- return 0;
+  for (i = 1;(t = hname[i]);++i)
+    if (hmatch(s,len,t))
+      return i;
+  return 0;
 }
 
-int hfield_valid(s,len)
-char *s;
-int len;
+int hfield_valid(char *s,int len)
+//char *s;
+//int len;
 {
- int i;
- int j;
- char ch;
+  int i;
+  int j;
+  char ch;
 
- for (j = 0;j < len;++j)
-   if (s[j] == ':')
-     break;
- if (j >= len) return 0;
- while (j)
+  for (j = 0;j < len;++j)
+    if (s[j] == ':')
+      break;
+  if (j >= len) return 0;
+  while (j)
   {
-   ch = s[j - 1];
-   if ((ch != ' ') && (ch != '\t'))
-     break;
-   --j;
+    ch = s[j - 1];
+    if ((ch != ' ') && (ch != '\t'))
+      break;
+    --j;
   }
- if (!j) return 0;
+  if (!j) return 0;
 
- for (i = 0;i < j;++i)
+  for (i = 0;i < j;++i)
   {
-   ch = s[i];
-   if (ch <= 32) return 0;
-   if (ch >= 127) return 0;
+    ch = s[i];
+    if (ch <= 32) return 0;
+    if (ch >= 127) return 0;
   }
- return 1;
+  return 1;
 }
 
-unsigned int hfield_skipname(s,len)
-char *s;
-int len;
+unsigned int hfield_skipname(char *s,int len)
+//char *s;
+//int len;
 {
- int i;
- char ch;
+  int i;
+  char ch;
 
- for (i = 0;i < len;++i)
-   if (s[i] == ':')
-     break;
- if (i < len) ++i;
- while (i < len)
+  for (i = 0;i < len;++i)
+    if (s[i] == ':')
+      break;
+  if (i < len) ++i;
+  while (i < len)
   {
-   ch = s[i];
-   if ((ch != '\t') && (ch != '\n') && (ch != '\r') && (ch != ' '))
-     break;
-   ++i;
+    ch = s[i];
+    if ((ch != '\t') && (ch != '\n') && (ch != '\r') && (ch != ' '))
+      break;
+    ++i;
   }
- return i;
+  return i;
 }
