7a7ee6b174fc93d854cac6feb92dd0e3ef71c7c0
Kai Peter <kp@openqmail.org>
2017-03-17 10:13:02 +0100

    qreceipt: Revision 20170317

--- qreceipt.c
+++ qreceipt.c
@@ -1,3 +1,8 @@
+/*  Revision 20160317, Kai Peter
+ *  - added braces to inner if-else block
+ *  - changed return type of main to int
+ *  - changed parameter declarations
+ */
 #include "sig.h"
 #include "env.h"
 #include "substdio.h"
@@ -9,8 +14,6 @@
 #include "hfield.h"
 #include "token822.h"
 #include "error.h"
-//#include "gen_alloc.h"
-//#include "gen_allocdefs.h"
 #include "headerbody.h"
 #include "exit.h"
 #include "open.h"
@@ -48,11 +51,11 @@ stralloc messageid = {0};
 stralloc sanotice = {0};
 
 int rwnotice(addr) token822_alloc *addr; { token822_reverse(addr);
- if (token822_unquote(&sanotice,addr) != 1) die_nomem();
- if (sanotice.len == str_len(target))
-   if (!str_diffn(sanotice.s,target,sanotice.len))
-     flagreceipt = 1;
- token822_reverse(addr); return 1; }
+  if (token822_unquote(&sanotice,addr) != 1) die_nomem();
+  if (sanotice.len == str_len(target))
+    if (!str_diffn(sanotice.s,target,sanotice.len))
+      flagreceipt = 1;
+  token822_reverse(addr); return 1; }
 
 struct qmail qqt;
 
@@ -60,41 +63,42 @@ stralloc quoted = {0};
 
 void finishheader()
 {
- char *qqx;
+  char *qqx;
 
- if (!flagreceipt) die_noreceipt();
- if (str_equal(returnpath,"")) die_noreceipt();
- if (str_equal(returnpath,"#@[]")) die_noreceipt();
+  if (!flagreceipt) die_noreceipt();
+  if (str_equal(returnpath,"")) die_noreceipt();
+  if (str_equal(returnpath,"#@[]")) die_noreceipt();
 
- if (!quote2(&quoted,returnpath)) die_nomem();
+  if (!quote2(&quoted,returnpath)) die_nomem();
 
- if (qmail_open(&qqt) == -1) die_fork();
+  if (qmail_open(&qqt) == -1) die_fork();
 
- qmail_puts(&qqt,"From: DELIVERY NOTICE SYSTEM <");
- qmail_put(&qqt,quoted.s,quoted.len);
- qmail_puts(&qqt,">\n");
- qmail_puts(&qqt,"To: <");
- qmail_put(&qqt,quoted.s,quoted.len);
- qmail_puts(&qqt,">\n");
- qmail_puts(&qqt,"Subject: success notice\n\
+  qmail_puts(&qqt,"From: DELIVERY NOTICE SYSTEM <");
+  qmail_put(&qqt,quoted.s,quoted.len);
+  qmail_puts(&qqt,">\n");
+  qmail_puts(&qqt,"To: <");
+  qmail_put(&qqt,quoted.s,quoted.len);
+  qmail_puts(&qqt,">\n");
+  qmail_puts(&qqt,"Subject: success notice\n\
 \n\
 Hi! This is the qreceipt program. Your message was delivered to the\n\
 following address: ");
- qmail_puts(&qqt,target);
- qmail_puts(&qqt,". Thanks for asking.\n");
- if (messageid.s)
+  qmail_puts(&qqt,target);
+  qmail_puts(&qqt,". Thanks for asking.\n");
+  if (messageid.s)
   {
-   qmail_puts(&qqt,"Your ");
-   qmail_put(&qqt,messageid.s,messageid.len);
+    qmail_puts(&qqt,"Your ");
+    qmail_put(&qqt,messageid.s,messageid.len);
   }
 
- qmail_from(&qqt,"");
- qmail_to(&qqt,returnpath);
- qqx = qmail_close(&qqt);
+  qmail_from(&qqt,"");
+  qmail_to(&qqt,returnpath);
+  qqx = qmail_close(&qqt);
 
- if (*qqx)
-   if (*qqx == 'D') die_qqperm();
-   else die_qqtemp();
+  if (*qqx) {
+    if (*qqx == 'D') die_qqperm();
+    else die_qqtemp();
+  }
 }
 
 stralloc hfbuf = {0};
@@ -102,30 +106,28 @@ token822_alloc hfin = {0};
 token822_alloc hfrewrite = {0};
 token822_alloc hfaddr = {0};
 
-void doheaderfield(h)
-stralloc *h;
+void doheaderfield(stralloc *h)
 {
- switch(hfield_known(h->s,h->len))
+  switch(hfield_known(h->s,h->len))
   {
-   case H_MESSAGEID:
-     if (!stralloc_copy(&messageid,h)) die_nomem();
-     break;
-   case H_NOTICEREQUESTEDUPONDELIVERYTO:
-     doordie(h,token822_parse(&hfin,h,&hfbuf));
-     doordie(h,token822_addrlist(&hfrewrite,&hfaddr,&hfin,rwnotice));
-     break;
+    case H_MESSAGEID:
+      if (!stralloc_copy(&messageid,h)) die_nomem();
+      break;
+    case H_NOTICEREQUESTEDUPONDELIVERYTO:
+      doordie(h,token822_parse(&hfin,h,&hfbuf));
+      doordie(h,token822_addrlist(&hfrewrite,&hfaddr,&hfin,rwnotice));
+      break;
   }
 }
 
-void dobody(h) stralloc *h; { ; }
+void dobody(stralloc *h) { ; }
 
-void main(argc,argv)
-int argc;
-char **argv;
+int main(int argc,char **argv)
 {
- sig_pipeignore();
- if (!(target = argv[1])) die_usage();
- if (!(returnpath = env_get("SENDER"))) die_usage();
- if (headerbody(subfdin,doheaderfield,finishheader,dobody) == -1) die_read();
- die_noreceipt();
+  sig_pipeignore();
+  if (!(target = argv[1])) die_usage();
+  if (!(returnpath = env_get("SENDER"))) die_usage();
+  if (headerbody(subfdin,doheaderfield,finishheader,dobody) == -1) die_read();
+  die_noreceipt();
+  return(0);  /* never reached */
 }
