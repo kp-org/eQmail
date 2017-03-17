7a86f47298710ec96c9b050f54d01b7e495e4933
Kai Peter <kp@openqmail.org>
2017-03-17 09:01:07 +0100

    qmail-qread: Revision 20160907

--- qmail-qread.c
+++ qmail-qread.c
@@ -1,5 +1,12 @@
+/*
+ *  Revision 20160907, Kai Peter
+ *  - changed return type of main to int
+ *  - added parentheses to while loop
+ *  - included <unistd.h>
+ */
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include "stralloc.h"
 #include "substdio.h"
 #include "subfd.h"
@@ -12,7 +19,6 @@
 #include "open.h"
 #include "datetime.h"
 #include "date822fmt.h"
-#include <unistd.h>		/* replace "readwrite.h" "exit.h" */
 #include "error.h"
 
 readsubdir rs;
@@ -55,120 +61,120 @@ datetime_sec qtime;
 int flagbounce;
 unsigned long size;
 
-unsigned int fmtstats(s)
-char *s;
+unsigned int fmtstats(char *s)
 {
- struct datetime dt;
- unsigned int len;
- unsigned int i;
+  struct datetime dt;
+  unsigned int len;
+  unsigned int i;
 
- len = 0;
- datetime_tai(&dt,qtime);
- i = date822fmt(s,&dt) - 7/*XXX*/; len += i; if (s) s += i;
- i = fmt_str(s," GMT  #"); len += i; if (s) s += i;
- i = fmt_ulong(s,id); len += i; if (s) s += i;
- i = fmt_str(s,"  "); len += i; if (s) s += i;
- i = fmt_ulong(s,size); len += i; if (s) s += i;
- i = fmt_str(s,"  <"); len += i; if (s) s += i;
- i = fmt_str(s,sender.s + 1); len += i; if (s) s += i;
- i = fmt_str(s,"> "); len += i; if (s) s += i;
- if (flagbounce)
+  len = 0;
+  datetime_tai(&dt,qtime);
+  i = date822fmt(s,&dt) - 7/*XXX*/; len += i; if (s) s += i;
+  i = fmt_str(s," GMT  #"); len += i; if (s) s += i;
+  i = fmt_ulong(s,id); len += i; if (s) s += i;
+  i = fmt_str(s,"  "); len += i; if (s) s += i;
+  i = fmt_ulong(s,size); len += i; if (s) s += i;
+  i = fmt_str(s,"  <"); len += i; if (s) s += i;
+  i = fmt_str(s,sender.s + 1); len += i; if (s) s += i;
+  i = fmt_str(s,"> "); len += i; if (s) s += i;
+  if (flagbounce)
   {
-   i = fmt_str(s," bouncing"); len += i; if (s) s += i;
+    i = fmt_str(s," bouncing"); len += i; if (s) s += i;
   }
 
- return len;
+  return len;
 }
 
 stralloc stats = {0};
 
 void out(s,n) char *s; unsigned int n;
 {
- while (n > 0)
+  while (n > 0)
   {
-   substdio_put(subfdout,((*s >= 32) && (*s <= 126)) ? s : "_",1);
-   --n;
-   ++s;
+    substdio_put(subfdout,((*s >= 32) && (*s <= 126)) ? s : "_",1);
+    --n;
+    ++s;
   }
 }
-void outs(s) char *s; { out(s,str_len(s)); }
-void outok(s) char *s; { substdio_puts(subfdout,s); }
+void outs(char *s) { out(s,str_len(s)); }
+void outok(char *s) { substdio_puts(subfdout,s); }
 
 void putstats()
 {
- if (!stralloc_ready(&stats,fmtstats(FMT_LEN))) die_nomem();
- stats.len = fmtstats(stats.s);
- out(stats.s,stats.len);
- outok("\n");
+  if (!stralloc_ready(&stats,fmtstats(FMT_LEN))) die_nomem();
+  stats.len = fmtstats(stats.s);
+  out(stats.s,stats.len);
+  outok("\n");
 }
 
 stralloc line = {0};
 
-void main()
+int main()
 {
- int channel;
- int match;
- struct stat st;
- int fd;
- substdio ss;
- int x;
+  int channel;
+  int match;
+  struct stat st;
+  int fd;
+  substdio ss;
+  int x;
 
- if (chdir(auto_qmail) == -1) die_chdir();
- if (chdir("queue") == -1) die_chdir();
- readsubdir_init(&rs,"info",die_opendir);
+  if (chdir(auto_qmail) == -1) die_chdir();
+  if (chdir("queue") == -1) die_chdir();
+  readsubdir_init(&rs,"info",die_opendir);
 
- while (x = readsubdir_next(&rs,&id))
-   if (x > 0)
+  while ((x = readsubdir_next(&rs,&id)))
+    if (x > 0)
     {
-     fmtqfn(fnmess,"mess/",id,1);
-     fmtqfn(fninfo,"info/",id,1);
-     fmtqfn(fnlocal,"local/",id,1);
-     fmtqfn(fnremote,"remote/",id,1);
-     fmtqfn(fnbounce,"bounce/",id,0);
+      fmtqfn(fnmess,"mess/",id,1);
+      fmtqfn(fninfo,"info/",id,1);
+      fmtqfn(fnlocal,"local/",id,1);
+      fmtqfn(fnremote,"remote/",id,1);
+      fmtqfn(fnbounce,"bounce/",id,0);
 
-     if (stat(fnmess,&st) == -1) { err(id); continue; }
-     size = st.st_size;
-     flagbounce = !stat(fnbounce,&st);
+      if (stat(fnmess,&st) == -1) { err(id); continue; }
+      size = st.st_size;
+      flagbounce = !stat(fnbounce,&st);
 
-     fd = open_read(fninfo);
-     if (fd == -1) { err(id); continue; }
-     substdio_fdbuf(&ss,read,fd,inbuf,sizeof(inbuf));
-     if (getln(&ss,&sender,&match,0) == -1) die_nomem();
-     if (fstat(fd,&st) == -1) { close(fd); err(id); continue; }
-     close(fd);
-     qtime = st.st_mtime;
+      fd = open_read(fninfo);
+      if (fd == -1) { err(id); continue; }
+      substdio_fdbuf(&ss,read,fd,inbuf,sizeof(inbuf));
+      if (getln(&ss,&sender,&match,0) == -1) die_nomem();
+      if (fstat(fd,&st) == -1) { close(fd); err(id); continue; }
+      close(fd);
+      qtime = st.st_mtime;
 
-     putstats();
+      putstats();
 
-     for (channel = 0;channel < 2;++channel)
+      for (channel = 0;channel < 2;++channel)
       {
-       fd = open_read(channel ? fnremote : fnlocal);
-       if (fd == -1)
-	{
-	 if (errno != error_noent)
-	   err(id);
-	}
-       else
+        fd = open_read(channel ? fnremote : fnlocal);
+        if (fd == -1)
         {
-         for (;;)
-	  {
-	   if (getln(&ss,&line,&match,0) == -1) die_nomem();
-	   if (!match) break;
-	   switch(line.s[0])
-	    {
-	     case 'D':
-	       outok("  done");
-	     case 'T':
-	       outok(channel ? "\tremote\t" : "\tlocal\t");
-	       outs(line.s + 1);
-	       outok("\n");
-	       break;
-	    }
-	  }
-         close(fd);
+          if (errno != error_noent)
+            err(id);
+        }
+        else
+        {
+          for (;;)
+          {
+          if (getln(&ss,&line,&match,0) == -1) die_nomem();
+          if (!match) break;
+          switch(line.s[0])
+            {
+            case 'D':
+              outok("  done");
+            case 'T':
+              outok(channel ? "\tremote\t" : "\tlocal\t");
+              outs(line.s + 1);
+              outok("\n");
+              break;
+            }
+          }
+          close(fd);
         }
       }
     }
 
- die(0);
+  die(0);
+  return(0);  /* never reached */
 }
