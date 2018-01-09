6ccdd96ef3ce3d3b34aebb27f12ded3b901fe659
Kai Peter <kp@openqmail.org>
2018-01-09 13:14:08 +0100

    mkconfig: replaced 'control' by 'etc'

--- mkconfig.sh
+++ mkconfig.sh
@@ -9,41 +9,41 @@ if [ ! "$DOMAIN" ] ; then
    echo -e "\n\033[1m >>> Couldn't determine DNS domain! Check your config!\033[0m\n" ; fi
 
 # create fundamental config files (if not exists)
-[ -f QMAIL/control/me ] || (
-  echo -e "Putting \033[33m"$FQDN"\033[0m into control/me..."
-  echo "$FQDN" > QMAIL/control/me
-  chmod 644 QMAIL/control/me
+[ -f QMAIL/etc/me ] || (
+  echo -e "Putting \033[33m"$FQDN"\033[0m into etc/me..."
+  echo "$FQDN" > QMAIL/etc/me
+  chmod 644 QMAIL/etc/me
   )
 
-[ -f QMAIL/control/locals ] || (
+[ -f QMAIL/etc/locals ] || (
   echo " "
-  echo -e "Putting \033[33m"$FQDN"\033[0m into control/locals..."
-  echo "$FQDN" > QMAIL/control/locals
+  echo -e "Putting \033[33m"$FQDN"\033[0m into etc/locals..."
+  echo "$FQDN" > QMAIL/etc/locals
   echo " "
   echo "If there are any other domain names that point to you, you will have to"
-  echo "add them to QMAIL/control/locals."
+  echo "add them to QMAIL/etc/locals."
   echo "You don\'t have to worry about aliases, i.e., domains with CNAME records."
   echo " "
   )
 
 if [ "$DOMAIN" ] ; then
-   [ -f QMAIL/control/defaultdomain ] || (
-     echo -e "Putting \033[33m"$DOMAIN"\033[0m into control/defaultdomain..."
-     echo "$DOMAIN" > QMAIL/control/defaultdomain
-     chmod 644 QMAIL/control/defaultdomain
+   [ -f QMAIL/etc/defaultdomain ] || (
+     echo -e "Putting \033[33m"$DOMAIN"\033[0m into etc/defaultdomain..."
+     echo "$DOMAIN" > QMAIL/etc/defaultdomain
+     chmod 644 QMAIL/etc/defaultdomain
      )
 
-   [ -f QMAIL/control/plusdomain ] || (
-     echo -e "Putting \033[33m"$DOMAIN"\033[0m into control/plusdomain..."
-     echo "$DOMAIN" > QMAIL/control/plusdomain
-     chmod 644 QMAIL/control/plusdomain
+   [ -f QMAIL/etc/plusdomain ] || (
+     echo -e "Putting \033[33m"$DOMAIN"\033[0m into etc/plusdomain..."
+     echo "$DOMAIN" > QMAIL/etc/plusdomain
+     chmod 644 QMAIL/etc/plusdomain
      )
 fi
 
-[ -f QMAIL/control/rcpthosts ] || (
-  echo "Copying QMAIL/control/locals to QMAIL/control/rcpthosts..."
-  cp QMAIL/control/locals QMAIL/control/rcpthosts
-  chmod 644 QMAIL/control/rcpthosts
+[ -f QMAIL/etc/rcpthosts ] || (
+  echo "Copying QMAIL/etc/locals to QMAIL/etc/rcpthosts..."
+  cp QMAIL/etc/locals QMAIL/etc/rcpthosts
+  chmod 644 QMAIL/etc/rcpthosts
   echo " "
   echo "Now eQmail will refuse to accept SMTP messages except to those hosts."
   echo "Make sure to change rcpthosts if you add hosts to locals or virtualdomains!"
@@ -51,55 +51,49 @@ fi
   )
 
 #********************************************************************************
-# create control/smtpplugins
-[ -f QMAIL/control/smtpplugins ] || \
+# create etc/smtpplugins
+[ -f QMAIL/etc/smtpplugins ] || \
 ( echo "Creating empty smtpplugins config file..."
 printf "
 # config file for qmail-spp\n#\n
-# pathes are relativ to QMAIL\n#\n" > QMAIL/control/smtpplugins
+# pathes are relativ to QMAIL\n#\n" > QMAIL/etc/smtpplugins
 printf "[connection]\n\n[helo]\n\n[mail]\n\n[rcpt]\n\n[data]\n\n#[auth]\n" \
-       >> QMAIL/control/smtpplugins
-chown root:qmail QMAIL/control/smtpplugins
-chmod 644 QMAIL/control/smtpplugins	)
-
-mkdir -p QMAIL/bin/plugins
-chgrp qmail QMAIL/bin/plugins
+       >> QMAIL/etc/smtpplugins
+chown root:qmail QMAIL/etc/smtpplugins
+chmod 644 QMAIL/etc/smtpplugins	)
 
 # create config file for qmail-bfque
-[ -f QMAIL/control/beforequeue ] || \
+[ -f QMAIL/etc/beforequeue ] || \
 ( echo "Creating config file beforequeue..."
-printf "#!/bin/sh
-# config file for qmail-bfqueue\n#\n
-# Enter commands here, like
+printf "QMAIL/bin/qmail-queue
 
-export QUEUE=\"\$BINDIR/qmail-queue\"
-np=\"\$BINDIR/qmail-queue\" ; exec \$np \"\$@\"
-"       >> QMAIL/control/beforequeue
-chown root:qmail QMAIL/control/beforequeue
-chmod 644 QMAIL/control/beforequeue     )
+See qmail-bfrmt(8), beforequeue(5) and qmail picture for details.
+"       >> QMAIL/etc/beforequeue )
+
 
 # create config file for qmail-bfrmt
-[ -f QMAIL/control/beforemote ] || \
+[ -f QMAIL/etc/beforemote ] || \
 ( echo "Creating config file beforemote..."
 printf "QMAIL/bin/qmail-remote
 
 See qmail-bfrmt(8), beforemote(5) and qmail picture for details.
-"       >> QMAIL/control/beforemote
-chown root:qmail QMAIL/control/beforemote
-chmod 644 QMAIL/control/beforemote     )
+"       >> QMAIL/etc/beforemote )
+
+chown root:qmail QMAIL/etc/beforemote QMAIL/etc/beforequeue
+chmod 644 QMAIL/etc/beforemote QMAIL/etc/beforequeue
 
 #********************************************************************************
 WITHTLS=$(cat conf.tmp | grep ^TLS=\"Yes\")
 if [ "$WITHTLS" = "TLS=\"Yes\"" ] ; then
    # create servercert.cnf and TLS/SSL keys and certificates
-   [ -f QMAIL/control/servercert.cnf ] || (
+   [ -f QMAIL/etc/servercert.cnf ] || (
      cat servercert.cnf | sed s{localhost{"$FQDN"{g \
-     > QMAIL/control/servercert.cnf
-     chmod 640 QMAIL/control/servercert.cnf
-     chown root:qmail QMAIL/control/servercert.cnf
+     > QMAIL/etc/servercert.cnf
+     chmod 640 QMAIL/etc/servercert.cnf
+     chown root:qmail QMAIL/etc/servercert.cnf
      )
-   mkdir -p QMAIL/control/tlshosts
-   [ -f QMAIL/control/servercert.pem ] || ( \
+   mkdir -p QMAIL/etc/tlshosts
+   [ -f QMAIL/etc/servercert.pem ] || ( \
      echo -e "\nCreating temporary keys and self-signed certificate ..."
      ./mkrsadhkeys
      ./mksrvrcerts
