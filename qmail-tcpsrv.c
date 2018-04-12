0e0170b2a8b2c0d7d0b976bf7d9b7bd5e7d2ecfc
Kai Peter <kp@openqmail.org>
2018-04-12 11:16:18 +0200

    qmail-tcpsrv: revision 20180412

--- qmail-tcpsrv.c
+++ qmail-tcpsrv.c
@@ -1,4 +1,11 @@
 /*
+ *  Revision 20180412, Kai Peter
+ *  - replaced 'strerr' by 'errmsg'
+ *  - replaced 'forcev6' and 'fakev4' by 'ipv4socket'
+ *  - removed "TCP6*" env vars
+ *  - added function 'socket_ipoptionskill'
+ *  Revision 20180131, Kai Peter
+ *  - changed call of function 'dns_name6()' --> 'dns_name()
  *  Revision 20180119, Kai Peter
  *  - prevent 'misleading-indentation' warning
 */
@@ -7,30 +14,26 @@
 #include <netdb.h>
 #include <unistd.h>
 #include "uint_t.h"
-#include "str.h"
-#include "byte.h"
-#include "fmt.h"
-#include "scan.h"
 #include "ip.h"
 #include "fd.h"
 #include "env.h"
 #include "prot.h"
 #include "open.h"
 #include "wait.h"
-#include "stralloc.h"
 #include "buffer.h"
-#include "error.h"
-#include "strerr.h"
+#include "errmsg.h"
 #include "getoptb.h"
 #include "pathexec.h"
-#include "socket.h"
+#include "qsocket.h"
 #include "ndelay.h"
 #include "remoteinfo.h"
 #include "rules.h"
 #include "sig.h"
 #include "dns.h"
+#include "qstrings.h"
 
-int forcev6 = 0;
+#define WHO "qmail-tcpsrv"
+
 int verbosity = 1;
 int flagkillopts = 1;
 int flagdelay = 1;
@@ -40,8 +43,8 @@ int flagremotehost = 1;
 int flagparanoid = 0;
 unsigned long timeout = 26;
 uint32 netif = 0;
-// 
-int ipv4socket = 0;
+
+int ipv4socket;
 
 static stralloc tcpremoteinfo;
 
@@ -60,7 +63,6 @@ static stralloc remotehostsa;
 char *remotehost = 0;
 
 char strnum[FMT_ULONG];
-char strnum2[FMT_ULONG];
 
 static stralloc tmp;
 static stralloc fqdn;
@@ -73,26 +75,14 @@ buffer b;
 
 /* ---------------------------- child */
 
-#define DROP "qmail-tcpsrv: warning: dropping connection, "
-
 int flagdeny = 0;
 int flagallownorules = 0;
 char *fnrules = 0;
 
-void drop_nomem(void)
-{
-  strerr_die2sys(111,DROP,"out of memory");
-}
-void cats(char *s)
-{
-  if (!stralloc_cats(&tmp,s)) drop_nomem();
-}
-void append(char *ch)
-{
-  if (!stralloc_append(&tmp,ch)) drop_nomem();
-}
-void safecats(char *s)
-{
+void cats(char *s) { if (!stralloc_cats(&tmp,s)) errmem; }
+void append(char *ch) { if (!stralloc_append(&tmp,ch)) errmem; }
+
+void safecats(char *s) {
   char ch;
   int i;
 
@@ -107,13 +97,15 @@ void safecats(char *s)
   }
   cats("...");
 }
-void env(const char *s,const char *t)
-{
-  if (!pathexec_env(s,t)) drop_nomem();
-}
-void drop_rules(void)
-{
-  strerr_die4sys(111,DROP,"unable to read ",fnrules,": ");
+void env(const char *s,const char *t) { if (!pathexec_env(s,t)) errmem; }
+void drop_rules(void) { errint(111,B("unable to read ",fnrules,": ")); }
+
+/* socket option: ipotionskill */
+int socket_ipoptionskill(int s) {
+  if (ipv4socket)
+    return setsockopt(s,IPPROTO_IP,1,(char *) 0,0); /* 1 == IP_OPTIONS */
+
+  return setsockopt(s,IPPROTO_IPV6,1,(char *) 0,0); /* 1 == IP_OPTIONS */
 }
 
 void found(char *data,unsigned int datalen)
@@ -141,20 +133,17 @@ void found(char *data,unsigned int datalen)
 
 void doit(int t)
 {
-  int fakev4=0;
   int j;
   uint32 scope_id;
 
-  if (!forcev6 && ip6_isv4mapped(remoteip))
-    fakev4=1;
-  if (fakev4)
+  if (ip6_isv4mapped(remoteip))
     remoteipstr[ip4_fmt(remoteipstr,remoteip+12)] = 0;
   else
     remoteipstr[ip6_fmt(remoteipstr,remoteip)] = 0;
 
   if (verbosity >= 2) {
     strnum[fmt_ulong(strnum,getpid())] = 0;
-    strerr_warn4("qmail-tcpsrv: pid ",strnum," from ",remoteipstr,0);
+    log(B("pid ",strnum," from ",remoteipstr,0));
   }
 
   if (flagkillopts)
@@ -165,38 +154,32 @@ void doit(int t)
   if (*banner) {
     buffer_init(&b,write,t,bspace,sizeof bspace);
     if (buffer_putsflush(&b,banner) == -1)
-      strerr_die2sys(111,DROP,"unable to print banner: ");
+      errint(EHARD,"unable to print banner: ");
   }
 
-  if (socket_local6(t,localip,&localport,&scope_id) == -1)
-    strerr_die2sys(111,DROP,"unable to get local address: ");
+  if (socket_local(t,localip,&localport,&scope_id) == -1)
+    errint(EHARD,"unable to get local address: ");
 
-  if (fakev4)
+  if (ip6_isv4mapped(localip))
     localipstr[ip4_fmt(localipstr,localip+12)] = 0;
   else
     localipstr[ip6_fmt(localipstr,localip)] = 0;
   remoteportstr[fmt_ulong(remoteportstr,remoteport)] = 0;
 
   if (!localhost)
-    if (dns_name6(&localhostsa,localip) == 0)
+    if (dns_name(&localhostsa,localip) == 0)
       if (localhostsa.len) {
-    if (!stralloc_0(&localhostsa)) drop_nomem();
+    if (!stralloc_0(&localhostsa)) errmem;
     localhost = localhostsa.s;
       }
-  env("PROTO",fakev4?"TCP":"TCP6");
-  env("TCPLOCALIP",localipstr);
-  localipstr[ip6_fmt(localipstr,localip)]=0;
-  env("TCP6LOCALIP",localipstr);
+  env("PROTO","TCP");
 
+  env("TCPLOCALIP",localipstr);
   env("TCPLOCALPORT",localportstr);
-  env("TCP6LOCALPORT",localportstr);
   env("TCPLOCALHOST",localhost);
-  env("TCP6LOCALHOST",localhost);
-  if (!fakev4 && scope_id)
-    env("TCP6INTERFACE",socket_getifname(scope_id));
 
   if (flagremotehost)
-    if (dns_name6(&remotehostsa,remoteip) == 0)
+    if (dns_name(&remotehostsa,remoteip) == 0)
       if (remotehostsa.len) {
     if (flagparanoid)
       if (dns_ip6(&tmp,&remotehostsa) == 0)
@@ -206,25 +189,20 @@ void doit(int t)
         break;
           }
     if (!flagparanoid) {
-      if (!stralloc_0(&remotehostsa)) drop_nomem();
+      if (!stralloc_0(&remotehostsa)) errmem;
       remotehost = remotehostsa.s;
     }
       }
   env("TCPREMOTEIP",remoteipstr);
-  remoteipstr[ip6_fmt(remoteipstr,remoteip)]=0;
-  env("TCP6REMOTEIP",remoteipstr);
   env("TCPREMOTEPORT",remoteportstr);
-  env("TCP6REMOTEPORT",remoteportstr);
   env("TCPREMOTEHOST",remotehost);
-  env("TCP6REMOTEHOST",remotehost);
 
   if (flagremoteinfo) {
     if (remoteinfo6(&tcpremoteinfo,remoteip,remoteport,localip,localport,timeout,netif) == -1)
       flagremoteinfo = 0;
-    if (!stralloc_0(&tcpremoteinfo)) drop_nomem();
+    if (!stralloc_0(&tcpremoteinfo)) errmem;
   }
   env("TCPREMOTEINFO",flagremoteinfo ? tcpremoteinfo.s : 0);
-  env("TCP6REMOTEINFO",flagremoteinfo ? tcpremoteinfo.s : 0);
 
   if (fnrules) {
     int fdrules;
@@ -234,22 +212,20 @@ void doit(int t)
       if (!flagallownorules) drop_rules();
     }
     else {
-      int fakev4=0;
       char* temp;
-      if (!forcev6 && ip6_isv4mapped(remoteip))
-    fakev4=1;
-      if (fakev4)
-    temp=remoteipstr+7;
+	  if (ip6_isv4mapped(remoteip))
+        temp=remoteipstr+7;
       else
-    temp=remoteipstr;
+        temp=remoteipstr;
       if (rules(found,fdrules,temp,remotehost,flagremoteinfo ? tcpremoteinfo.s : 0) == -1) drop_rules();
       close(fdrules);
+//      log(B("checking tcp rules for ",remotehost,": pass"));
     }
   }
 
   if (verbosity >= 2) {
     strnum[fmt_ulong(strnum,getpid())] = 0;
-    if (!stralloc_copys(&tmp,"qmail-tcpsrv: ")) drop_nomem();
+    if (!stralloc_copys(&tmp,"qmail-tcpsrv: ")) errmem;
     safecats(flagdeny ? "deny" : "ok");
     cats(" "); safecats(strnum);
     cats(" "); if (localhost) safecats(localhost);
@@ -257,6 +233,7 @@ void doit(int t)
     cats(":"); safecats(localportstr);
     cats(" "); if (remotehost) safecats(remotehost);
     cats(":"); safecats(remoteipstr);
+//    cats(":<remote info>"); if (flagremoteinfo) safecats(tcpremoteinfo.s);
     cats(":"); if (flagremoteinfo) safecats(tcpremoteinfo.s);
     cats(":"); safecats(remoteportstr);
     cats("\n");
@@ -267,27 +244,13 @@ void doit(int t)
 }
 
 
-
 /* ---------------------------- parent */
 
-#define FATL "qmail-tcpsrv: fatal: "
-
-void usage(void)
-{
-  strerr_warn1("\
-qmail-tcpsrv: usage: qmail-tcpsrv \
-[ -461UXpPhHrRoOdDqQv ] \
-[ -c limit ] \
-[ -x rules.cdb ] \
-[ -B banner ] \
-[ -g gid ] \
-[ -u uid ] \
-[ -b backlog ] \
-[ -l localname ] \
-[ -t timeout ] \
-[ -I interface ] \
-host port program",0);
-  _exit(100);
+void usage(void) {
+  errlog(0,ERROR,B("usage: qmail-tcpsrv [ -41UXpPhHrRoOdDqQv ] \
+[ -c limit ] [ -x rules.cdb ] [ -B banner ] \
+[ -g gid ] [ -u uid ] [ -b backlog ] [ -l localname ] \
+[ -t timeout ] [ -I interface ] host port program"));
 }
 
 unsigned long limit = 40;
@@ -298,12 +261,9 @@ unsigned long backlog = 20;
 unsigned long uid = 0;
 unsigned long gid = 0;
 
-void printstatus(void)
-{
+void printstatus(void) {
   if (verbosity < 2) return;
-  strnum[fmt_ulong(strnum,numchildren)] = 0;
-  strnum2[fmt_ulong(strnum2,limit)] = 0;
-  strerr_warn4("qmail-tcpsrv: status: ",strnum,"/",strnum2,0);
+  log(B("status: ",fmtnum(numchildren),"/",fmtnum(limit)));
 }
 
 void sigterm() { _exit(0); }
@@ -315,9 +275,7 @@ void sigchld()
 
   while ((pid = wait_nohang(&wstat)) > 0) {
     if (verbosity >= 2) {
-      strnum[fmt_ulong(strnum,pid)] = 0;
-      strnum2[fmt_ulong(strnum2,wstat)] = 0;
-      strerr_warn4("qmail-tcpsrv: end ",strnum," status ",strnum2,0);
+      errlog(0,NOTICE,B("end ",fmtnum(pid)," status ",fmtnum(wstat)));
     }
     if (numchildren) { --numchildren; } printstatus();
   }
@@ -334,7 +292,7 @@ int main(int argc,char **argv)
   int s;
   int t;
 
-  while ((opt = getopt(argc,argv,"46dDvqQhHrR1UXx:t:u:g:l:b:B:c:I:pPoO")) != opteof)
+  while ((opt = getopt(argc,argv,"4dDvqQhHrR1UXx:t:u:g:l:b:B:c:I:pPoO")) != opteof)
     switch(opt) {
       case 'b': scan_ulong(optarg,&backlog); break;
       case 'c': scan_ulong(optarg,&limit); break;
@@ -363,7 +321,7 @@ int main(int argc,char **argv)
       case '1': flag1 = 1; break;
 //      case '4': noipv6 = 1; break;
       case '4': ipv4socket = 1; break;
-      case '6': forcev6 = 1; break;
+//      case '6': forcev6 = 1; break;
       case 'l': localhost = optarg; break;
       default: usage();
     }
@@ -384,7 +342,7 @@ int main(int argc,char **argv)
   else {
     se = getservbyname(x,"tcp");
     if (!se)
-      strerr_die3x(111,FATAL,"unable to figure out port number for ",x);
+      errint(EHARD,B("unable to figure out port number for ",x));
     uint16_unpack_big((char*)&se->s_port,&localport);
   }
 
@@ -398,33 +356,34 @@ int main(int argc,char **argv)
   if (str_equal(hostname,"0")) {
     byte_zero(localip,sizeof localip);
   } else {
-    if (!stralloc_copys(&tmp,hostname))
-      strerr_die2x(111,FATAL,"out of memory");
+    if (!stralloc_copys(&tmp,hostname)) errmem;
     if (dns_ip6_qualify(&addresses,&fqdn,&tmp) == -1)
-      strerr_die4sys(111,FATAL,"temporarily unable to figure out IP address for ",hostname,": ");
+      errint(EHARD,B("temporarily unable to figure out IP address for ",hostname,": "));
     if (addresses.len < 16)
-      strerr_die3x(111,FATAL,"no IP address for ",hostname);
+	  errint(EHARD,B("no IP address for ",hostname));
     byte_copy(localip,16,addresses.s);
     if (ip6_isv4mapped(localip))
-//      noipv6=1;
       ipv4socket = 1;
   }
 
-  s = socket_tcp6();
+  s = socket_tcp();
   if (s == -1)
-    strerr_die2sys(111,FATAL,"unable to create socket: ");
-  if (socket_bind6_reuse(s,localip,localport,netif) == -1)
-    strerr_die2sys(111,FATAL,"unable to bind: ");
-  if (socket_local6(s,localip,&localport,&netif) == -1)
-    strerr_die2sys(111,FATAL,"unable to get local address: ");
+    errint(EHARD,"unable to create socket: ");
+  if (socket_bind_reuse(s,localip,localport,netif) == -1)
+    errint(EHARD,"unable to bind: ");
+
+  if (!ipv4socket) ipv4socket = ip6_isv4mapped(localip);
+
+  if (socket_local(s,localip,&localport,&netif) == -1)
+    errint(EHARD,"unable to get local address: ");
   if (socket_listen(s,backlog) == -1)
-    strerr_die2sys(111,FATAL,"unable to listen: ");
+    errint(EHARD,"unable to listen: ");
   ndelay_off(s);
 
   if (gid) if (prot_gid(gid) == -1)
-    strerr_die2sys(111,FATAL,"unable to set gid: ");
+    errint(EHARD,"unable to set gid: ");
   if (uid) if (prot_uid(uid) == -1)
-    strerr_die2sys(111,FATAL,"unable to set uid: ");
+    errint(EHARD,"unable to set uid: ");
 
 
   localportstr[fmt_ulong(localportstr,localport)] = 0;
@@ -443,26 +402,26 @@ int main(int argc,char **argv)
     while (numchildren >= limit) sig_pause();
 
     sig_unblock(sig_child);
-    t = socket_accept6(s,remoteip,&remoteport,&netif);
+    t = socket_accept(s,remoteip,&remoteport,&netif);
     sig_block(sig_child);
 
     if (t == -1) continue;
     ++numchildren; printstatus();
- 
+
     switch(fork()) {
       case 0:
         close(s);
         doit(t);
         if ((fd_move(0,t) == -1) || (fd_copy(1,0) == -1))
-      strerr_die2sys(111,DROP,"unable to set up descriptors: ");
+          errint(EHARD,"unable to set up descriptors: ");
         sig_uncatch(sig_child);
         sig_unblock(sig_child);
         sig_uncatch(sig_term);
         sig_uncatch(sig_pipe);
         pathexec(argv);
-    strerr_die4sys(111,DROP,"unable to run ",*argv,": ");
+        errint(EHARD,B("unable to run ",*argv,": "));
       case -1:
-        strerr_warn2(DROP,"unable to fork: ",&strerr_sys);
+        errlog(ESOFT,NOTICE,"unable to fork: ");
         --numchildren; printstatus();
     }
     close(t);
